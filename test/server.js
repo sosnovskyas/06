4847b4d025305c1edab6b9d37462a819529458df
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-12 21:33:46 +0300

    update test

--- test/server.js
+++ test/server.js
@@ -1,4 +1,4 @@
-const app = require('../index');
+const app = require('../app');
 const request = require('request-promise').defaults({
   resolveWithFullResponse: true,
   simple: false
@@ -12,9 +12,7 @@ const User = require('../models/user');
 
 const serverPath = `http://${config.host}:${config.port}`;
 
-
-
-describe("User REST API", async function() {
+describe("User REST API", async function () {
 
   let existingUserData = {
     email: "john@test.ru",
@@ -27,21 +25,21 @@ describe("User REST API", async function() {
   let existingUser;
 
   before(done => {
-    server = app.listen(3000, done);
+    server = app.listen(config.port, done);
   });
 
   after(done => {
     server.close(done);
   });
 
-  beforeEach(async function() {
+  beforeEach(async function () {
     // load fixtures
     await User.remove({});
     existingUser = await User.create(existingUserData);
   });
 
-  describe("POST /users", async function() {
-    it("creates a user", async function() {
+  describe("POST /users", async function () {
+    it.only("creates a user", async function () {
       let response = await request({
         method: 'post',
         uri: `${serverPath}/users`,
@@ -52,7 +50,7 @@ describe("User REST API", async function() {
       response.body.email.should.eql(newUserData.email);
     });
 
-    it("throws if email already exists", async function() {
+    it("throws if email already exists", async function () {
       let response = await request({
         method: 'post',
         uri: `${serverPath}/users`,
@@ -63,7 +61,7 @@ describe("User REST API", async function() {
       response.body.errors.email.should.exist;
     });
 
-    it("throws if email not valid", async function() {
+    it("throws if email not valid", async function () {
       let response = await request({
         method: 'post',
         uri: `${serverPath}/users`,
@@ -77,40 +75,40 @@ describe("User REST API", async function() {
 
   });
 
-  describe("GET /user/:userById", async function() {
-    it("gets the user by id", async function() {
+  describe("GET /user/:userById", async function () {
+    it("gets the user by id", async function () {
       let response = await request.get(`${serverPath}/users/${existingUser._id}`);
       JSON.parse(response.body).email.should.exist;
       response.statusCode.should.eql(200);
       response.headers['content-type'].should.match(/application\/json/);
     });
 
-    it("returns 404 if user does not exist", async function() {
+    it("returns 404 if user does not exist", async function () {
       let response = await request.get(`${serverPath}/users/55b693486e02c26010ef0000`);
       response.statusCode.should.eql(404);
     });
 
-    it("returns 404 if invalid id", async function() {
+    it("returns 404 if invalid id", async function () {
       let response = await request.get(`${serverPath}/users/kkkkk`);
       response.statusCode.should.eql(404);
     });
   });
 
-  describe("DELETE /user/:userById", async function() {
-    it("removes user", async function() {
+  describe("DELETE /user/:userById", async function () {
+    it("removes user", async function () {
       let response = await request.del(getURL('/users/' + existingUser._id));
       response.statusCode.should.eql(200);
       let users = await User.find({}).exec();
       users.length.should.eql(0);
     });
 
-    it("returns 404 if the user does not exist", async function() {
+    it("returns 404 if the user does not exist", async function () {
       let response = await request.del(getURL('/users/55b693486e02c26010ef0000'));
       response.statusCode.should.eql(404);
     });
   });
 
-  it("GET /users gets all users", async function() {
+  it("GET /users gets all users", async function () {
     let response = await request.get(getURL('/users'));
     response.statusCode.should.eql(200);
     response.headers['content-type'].should.match(/application\/json/);
