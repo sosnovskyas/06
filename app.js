6a82c832fc2b2840e0dfb927a99a381821acd89d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-12 21:34:02 +0300

    refact

--- app.js
+++ app.js
@@ -11,5 +11,3 @@ const mongoose = require('./libs/mongoose');
 const middlewares = fs.readdirSync(path.join(__dirname, 'middlewares')).sort();
 
 middlewares.forEach(middleware => require('./middlewares/' + middleware).init(app));
-
-//
